4a3399b6c2f415b34f2a17eb0c52922e29be4baa
yehao li <469826095@qq.com>
2022-10-05 19:31:47 +0800

    camera ready

--- lstm.py
+++ lstm.py
@@ -1,29 +1,95 @@
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.layers import LSTM
-from pandas_datareader import DataReader
+# For reading stock data from stooq.com
+from pandas_datareader.data import DataReader
+# For plotting
+import matplotlib.pyplot as plt
+# For normalizing data
 from sklearn.preprocessing import MinMaxScaler
+# For building the model
+from keras.models import Sequential
+from keras.layers import Dense, LSTM, GRU
+import numpy as np
+# For time stamps
+from datetime import datetime
+import tensorflow as tf
 
-# read data
-print("Loading data...")
-df = DataReader("AAPL", data_source="yahoo", start="2020-01-01", end="2022-09-01")
-df = df.sort_index(ascending=True, axis=0)
-# Split the data into training and testing sets
+# Get the stock quote
+df = DataReader('AAPL', data_source='stooq', start='2012-01-01', end=datetime.now())
+df = df.iloc[::-1]
+
+# Create a new dataframe with only the 'Close' column
+data = df.filter(['Close'])
+# Convert the dataframe to a numpy array
+dataset = data.values
+# Get the number of rows to train the model on
+training_data_len = int(np.ceil(len(dataset) * .95))
+
+# scale the data
 scaler = MinMaxScaler(feature_range=(0, 1))
-scaled_data = scaler.fit_transform(df)
+scaled_data = scaler.fit_transform(dataset)
 
+# Create the training data set
+# Create the scaled training data set
+train_data = scaled_data[0:int(training_data_len), :]
+# Split the data into x_train and y_train data sets
+x_train = []
+y_train = []
+
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+
+# Convert the x_train and y_train to numpy arrays
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+# Reshape the data
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 # Build the LSTM model
-def build_model(inputs, output_size, neurons, activ_func="linear",
-                dropout=0.25, loss="mae", optimizer="adam"):
-    model = Sequential()
-    model.add(LSTM(neurons, input_shape=(inputs.shape[1], inputs.shape[2])))
-    model.add(Dropout(dropout))
-    model.add(Dense(units=output_size))
-    model.add(Activation(activ_func))
-    model.compile(loss=loss, optimizer=optimizer)
-    return model
+model = Sequential()
+model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
 
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
 
-# lstmModel = build_model(trainX, output_size=1, neurons=20)
-# lstmModel.fit(trainX, trainY, epochs=10, batch_size=1, verbose=2)
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+# Create the testing data set
+# Create a new array containing scaled values from index 1543 to 2002
+test_data = scaled_data[training_data_len-60:, :]
+# Create the data sets x_test and y_test
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(test_data)):
+    x_test.append(test_data[i-60:i, 0])
+
+# Convert the data to a numpy array
+x_test = np.array(x_test)
+
+# Reshape the data
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# Get the models predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)
+
+# Get the root mean squared error (RMSE)
+rmse = np.sqrt(np.mean(((predictions - y_test) ** 2)))
+print(rmse)
+
+# Plot the data
+train = data[:training_data_len]
+valid = data[training_data_len:]
+valid['Predictions'] = predictions
+# Visualize the data
+plt.figure(figsize=(16, 8))
+plt.title('Model')
+plt.xlabel('Date', fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.plot(train['Close'])
+plt.plot(valid[['Close', 'Predictions']])
+plt.legend(['Train', 'Val', 'Predictions'], loc='lower right')
+plt.show()
