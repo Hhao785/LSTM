e4a8b40da9dbba5be4f5a6db3f86e58e634b0e57
yehao li <469826095@qq.com>
2022-10-06 15:59:51 +0800

    add plot model

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+
 # LSTM
-for presentation
+
+This repo contains the code for the presentation of LSTM. LSTM is a type of RNN that is able to remember information for a long period of time. It is able to do so by using gates. The gates are used to decide what information to throw away and what information to remember. The gates are controlled by a sigmoid neural network which outputs a number between 0 and 1, where 0 means "completely forget" and 1 means "completely remember". The sigmoid is used to make sure that the gates are either completely open or completely closed. The gates are used to control the flow of information. The gates are:
+
+## Requirements
--- lstm.py
+++ lstm.py
@@ -5,12 +5,14 @@ import matplotlib.pyplot as plt
 # For normalizing data
 from sklearn.preprocessing import MinMaxScaler
 # For building the model
+from keras.utils.vis_utils import plot_model
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, GRU
 import numpy as np
 # For time stamps
 from datetime import datetime
-import tensorflow as tf
+import os
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 
 # Get the stock quote
 df = DataReader('AAPL', data_source='stooq', start='2012-01-01', end=datetime.now())
@@ -44,19 +46,6 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 # Reshape the data
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
-# Build the LSTM model
-model = Sequential()
-model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(LSTM(64, return_sequences=False))
-model.add(Dense(25))
-model.add(Dense(1))
-
-# Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
-
-# Train the model
-model.fit(x_train, y_train, batch_size=1, epochs=1)
-
 # Create the testing data set
 # Create a new array containing scaled values from index 1543 to 2002
 test_data = scaled_data[training_data_len-60:, :]
@@ -71,6 +60,20 @@ x_test = np.array(x_test)
 
 # Reshape the data
 x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(128, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(64, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
+
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
 
 # Get the models predicted price values
 predictions = model.predict(x_test)
